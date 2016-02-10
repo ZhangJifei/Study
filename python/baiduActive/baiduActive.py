e3da5d6e17d5b329127e138ebc6960fa527900e9
ZhangJifei <77651946@qq.com>
2016-02-10 18:58:14 +0800

    With Chinese

---
+++ python/baiduActive/baiduActive.py
@@ -0,0 +1,11 @@
+﻿from urllib import request
+from urllib import parse
+
+url = "http://www.baidu.com/s?wd=" + parse.quote('中文')
+
+with request.urlopen(url) as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))

485f745f9fce8dcdeddd08be7198572904f1b9dd
ZhangJifei <77651946@qq.com>
2016-02-17 22:11:52 +0800

    backup 0217

--- python/baiduActive/mainSearch.py
+++ python/baiduActive/mainSearch.py
@@ -1,14 +1,15 @@
 from baiduActive import *
 import time
+import random
 from myLogging import *
 
 words_array = ('祛痘 西南交大', '祛痘 西南交通大学', '痘痘 西南交大', '痘痘 西南交通大学')
 page_array = (0, 10, 20)
 
-for i in range(100):
+for i in range(200):
     for words in words_array:
         for page in page_array:
             baidu_search(words, page)
             time.sleep(10)
-    logger.info ('Start to trigger search')
-    time.sleep(3600)
+    logger.info ('Complete the %d th search' % (i+1))
+    time.sleep(3500+random.randint(0, 200))

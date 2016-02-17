62cdc6d02a9519b9d62b6ae48395c439b597528b
ZhangJifei <77651946@qq.com>
2016-02-17 22:55:08 +0800

    urlopen + header

--- python/baiduActive/baiduActive.py
+++ python/baiduActive/baiduActive.py
@@ -4,14 +4,16 @@ import re
 import logging
 from myLogging import *
 
+
 def baidu_search(words, page):
     url = "http://www.baidu.com/s?wd=" + parse.quote(words) + "&pn=%s"%(str(page))
     url_58_temp = "http://cd.58.com/mianbumeir/23435086794014x.shtml?PGTID=0d000000-0301-77a5-a9fc-d94212c8db35&ClickID=1"
     url_dianping_temp = "http://www.dianping.com/shop/23583529"
 
     try:
+        headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
 
-        with request.urlopen(url) as f:
+        with request.urlopen(request.Request(url=url, headers=headers)) as f:
             data = f.readlines()
 
             for line in data:
@@ -21,22 +23,22 @@ def baidu_search(words, page):
                     url_temp = m.group(2)
                 if re.match(r'.*瑶芳专业.*',line):
                     logger.info (url_temp)
-                    with request.urlopen(url_temp) as f_temp:pass
+                    with request.urlopen(request.Request(url=url_temp, headers=headers)) as f_temp:pass
 
                     # special access to the link for 大众点评
                     if re.match(r'.*大众点评.*',line):
                         logger.info (url_dianping_temp)
-                        with request.urlopen(url_dianping_temp) as url_dianping_temp:pass
+                        with request.urlopen(request.Request(url=url_dianping_temp, headers=headers)) as f_dianping_temp:pass
 
                     # special access to the link for 58
                     if re.match(r'.*成都58同城.*',line):
                         logger.info (url_58_temp)
-                        with request.urlopen(url_58_temp) as f_58_temp:pass
+                        with request.urlopen(request.Request(url=url_58_temp, headers=headers)) as f_58_temp:pass
 
     except Exception as e:
         logging.exception(e)
         if re.match(r'.*大众点评.*',line):
             logger.info (url_dianping_temp)
-            with request.urlopen(url_dianping_temp) as url_dianping_temp:pass
+            with request.urlopen(request.Request(url=url_dianping_temp, headers=headers)) as f_dianping_temp:pass
 
 
